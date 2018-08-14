9cd7b72ba0764f0b4a1ad792e604667c18ae751e
MarcDorval <marc.dorval@silabs.com>
2018-08-14 09:37:49 +0200

    Update update_1.2.8.sh

--- update_1.2.8.sh
+++ update_1.2.8.sh
@@ -5,5 +5,7 @@ REPO=https://github.com/MarcDorval/wofo.git
 # Run with:
 #  curl https://raw.githubusercontent.com/MarcDorval/wofo/master/pi/update_1.2.8.sh | sudo sh
 
+echo "Silicon Labs update script for WFx200 WiFi parts"
+
 ! grep -q 'NAME="Raspbian GNU/Linux"' /etc/os-release && echo "You must run this script from a Raspberry" && exit 1
 [ -z "$SUDO_USER" ] && echo "This script must be run with sudo" && exit 1
