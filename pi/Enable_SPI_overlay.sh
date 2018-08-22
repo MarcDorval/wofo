26991d40893fd9f8636ac214b24eefbd9b2636df
MarcDorval <marc.dorval@silabs.com>
2018-08-22 17:03:38 +0200

    Update Enable_SPI_overlay.sh
    More robust sed: can cope with spaces

--- pi/Enable_SPI_overlay.sh
+++ pi/Enable_SPI_overlay.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Enabling SPI overlay
-sudo sed -i~ 's/^#dtoverlay=wfx-spi/dtoverlay=wfx-spi/m' /boot/config.txt
+sudo sed -i~ 's/^\s*#\s*dtoverlay=wfx-spi/dtoverlay=wfx-spi/m' /boot/config.txt
