de33d03806989c5492794f64c9b4c4182c0deef4
MarcDorval <marc.dorval@silabs.com>
2018-08-22 16:51:23 +0200

    Correcting 'sdio' to 'wfx-sdio'

--- pi/Enable_SDIO_overlay.sh
+++ pi/Enable_SDIO_overlay.sh
@@ -3,4 +3,4 @@
 # Now using wfx-sdio overlay instead of sdio overlay
 sudo sed -i~ 's/dtoverlay=sdio/dtoverlay=wfx-sdio/m' /boot/config.txt
 # Enabling SDIO overlay
-sudo sed -i~ 's/^#dtoverlay=sdio/dtoverlay=sdio/m' /boot/config.txt
+sudo sed -i~ 's/^#dtoverlay=wfx-sdio/dtoverlay=wfx-sdio/m' /boot/config.txt
